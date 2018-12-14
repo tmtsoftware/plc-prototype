63dd8a41200b205acbf81a044577bcd6cb2db606
smichael1 <smichael1@cox.net>
2018-12-14 08:23:53 -0800

    Added library for javaplc, first tests of command service

--- plc-prototype-assembly/src/main/java/org/tmt/tel/plcprototypeassembly/JPlcprototypeAssemblyHandlers.java
+++ plc-prototype-assembly/src/main/java/org/tmt/tel/plcprototypeassembly/JPlcprototypeAssemblyHandlers.java
@@ -1,15 +1,26 @@
 package org.tmt.tel.plcprototypeassembly;
 
 import akka.actor.typed.javadsl.ActorContext;
+import csw.command.api.javadsl.ICommandService;
+import csw.command.client.CommandServiceFactory;
 import csw.command.client.messages.TopLevelActorMessage;
+import csw.command.client.models.framework.ComponentInfo;
 import csw.framework.javadsl.JComponentHandlers;
 import csw.framework.models.JCswContext;
-import csw.location.api.models.TrackingEvent;
+import csw.location.api.javadsl.JComponentType;
+import csw.location.api.models.*;
 import csw.logging.javadsl.ILogger;
+import csw.params.commands.CommandName;
 import csw.params.commands.CommandResponse;
 import csw.params.commands.ControlCommand;
+import csw.params.commands.Setup;
+import csw.params.core.models.ObsId;
+import csw.params.core.models.Prefix;
 
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import akka .util.Timeout;
 
 /**
  * Domain specific logic should be written in below handlers.
@@ -23,17 +34,20 @@ public class JPlcprototypeAssemblyHandlers extends JComponentHandlers {
 
     private final JCswContext cswCtx;
     private final ILogger log;
+    ICommandService hcd;
+    private ActorContext<TopLevelActorMessage> actorContext;
 
     JPlcprototypeAssemblyHandlers(ActorContext<TopLevelActorMessage> ctx,JCswContext cswCtx) {
         super(ctx, cswCtx);
         this.cswCtx = cswCtx;
         this.log = cswCtx.loggerFactory().getLogger(getClass());
+        this.actorContext = ctx;
     }
 
     @Override
     public CompletableFuture<Void> jInitialize() {
-    log.info("Initializing plcprototype assembly...");
-    return CompletableFuture.runAsync(() -> {
+        log.info("Initializing plcprototype assembly...");
+        return CompletableFuture.runAsync(() -> {
 
         });
     }
@@ -48,6 +62,34 @@ public class JPlcprototypeAssemblyHandlers extends JComponentHandlers {
     @Override
     public void onLocationTrackingEvent(TrackingEvent trackingEvent) {
 
+        if (trackingEvent instanceof LocationUpdated) {
+            LocationUpdated updated = (LocationUpdated) trackingEvent;
+            Location location = updated.location();
+            hcd = CommandServiceFactory.jMake((AkkaLocation) (location), actorContext.getSystem());
+            System.out.println("hcd located = " + hcd);
+
+            // as a test, let's read the PLC
+
+            Setup sc = new Setup(new Prefix("prototypeAssembly"), new CommandName("read"), Optional.of(new ObsId("Obs001")));
+            Timeout timeout = new Timeout(100, TimeUnit.SECONDS);
+            CompletableFuture<CommandResponse.SubmitResponse> immediateCommandF =
+                    hcd.submit(sc, timeout)
+                            .thenApply(
+                                    response -> {
+                                        if (response instanceof CommandResponse.CompletedWithResult) {
+                                            //do something with completed result
+
+                                            System.out.println("hcd command completed!");
+
+
+                                        } else {
+                                            System.out.println("unexpected response = " + response);
+                                        }
+                                        return response;
+                                    }
+                            );
+
+        }
     }
 
     @Override
--- plc-prototype-hcd/src/main/java/org/tmt/tel/plcprototypehcd/JPlcprototypeHcdHandlers.java
+++ plc-prototype-hcd/src/main/java/org/tmt/tel/plcprototypehcd/JPlcprototypeHcdHandlers.java
@@ -9,6 +9,10 @@ import csw.logging.javadsl.ILogger;
 import csw.params.commands.CommandResponse;
 import csw.params.commands.ControlCommand;
 
+import csw.params.core.generics.Key;
+import csw.params.core.generics.Parameter;
+import csw.params.core.models.Id;
+import csw.params.core.models.Prefix;
 import org.tmt.tel.javaplc.ABPlcioMaster;
 import org.tmt.tel.javaplc.IABPlcioMaster;
 import org.tmt.tel.javaplc.IPlcioCall;
@@ -19,6 +23,9 @@ import org.tmt.tel.javaplc.PlcTag;
 import org.tmt.tel.javaplc.PlcioPcFormat;
 import org.tmt.tel.javaplc.TagItem;
 
+import csw.params.commands.Result;
+
+import csw.params.javadsl.JKeyType;
 
 
 import java.util.concurrent.CompletableFuture;
@@ -35,6 +42,7 @@ public class JPlcprototypeHcdHandlers extends JComponentHandlers {
 
     private final JCswContext cswCtx;
     private final ILogger log;
+    IABPlcioMaster master;
 
     JPlcprototypeHcdHandlers(ActorContext<TopLevelActorMessage> ctx,JCswContext cswCtx) {
         super(ctx, cswCtx);
@@ -47,131 +55,9 @@ public class JPlcprototypeHcdHandlers extends JComponentHandlers {
     log.info("Initializing plcprototype HCD...");
     return CompletableFuture.runAsync(() -> {
 
-        try {
-            log.info("Initializing ABPlcioMaster...");
-            IABPlcioMaster master = new ABPlcioMaster();
-            log.info("Completed...");
-
-
-            // perform a test read to prove it can be done within the HCD
-            PlcioCall plcioCallOpen1 = new PlcioCall(IPlcioCall.PlcioMethodName.PLC_OPEN, "cip 192.168.1.20",
-                    "Scott_R_Conn", 0);
-            PlcioCall plcioCallOpen2 = new PlcioCall(IPlcioCall.PlcioMethodName.PLC_OPEN, "cip 192.168.1.20",
-                    "Scott_D_Conn", 0);
-
-
-            TagItem tagItem1 = new TagItem("myRealValue", IPlcTag.PropTypes.REAL.getTypeString(), 0,
-                    PlcioPcFormat.TYPE_R, 0, 0);
-
-            TagItem tagItem2 = new TagItem("myDecimalValue", IPlcTag.PropTypes.INTEGER.getTypeString(), 0,
-                    PlcioPcFormat.TYPE_J, 0, 0);
-
-            TagItem tagItem3 = new TagItem("myBooleanValue", IPlcTag.PropTypes.BOOLEAN.getTypeString(), 0,
-                    PlcioPcFormat.TYPE_C, 0, 0);
-
-            TagItem tagItem4 = new TagItem("myRealValue", IPlcTag.PropTypes.REAL.getTypeString(), 0,
-                    PlcioPcFormat.TYPE_R, 0, 0);
-
-
-            TagItem[] tagItems1 = {tagItem1};
-            TagItem[] tagItems2 = {tagItem2};
-            TagItem[] tagItems3 = {tagItem3};
-            TagItem[] tagItems4 = {tagItem4};
-
-
-            PlcTag plcTag1 = new PlcTag("Scott_R", "" + PlcioPcFormat.TYPE_R,
-                    10000, 1, 4, tagItems1);
-            PlcTag plcTag2 = new PlcTag("Scott_D", "" + PlcioPcFormat.TYPE_J,
-                    10000, 1, 4, tagItems2);
-            PlcTag plcTag3 = new PlcTag("Scott_B", "" + PlcioPcFormat.TYPE_C,
-                    10000, 1, 1, tagItems3);
-
-            PlcTag plcTag4 = new PlcTag("Scott_R", "" + PlcioPcFormat.TYPE_R,
-                    10000, 1, 4, tagItems4);
-            String[] newValues = {"5678.0"};
-            plcTag4.setMemberValues(newValues);
-
-
-            PlcioCall plcioCallRead1 = new PlcioCall(IPlcioCall.PlcioMethodName.PLC_READ, "cip 192.168.1.20",
-                    "Scott_R_Conn", 0, plcTag1);
-
-            PlcioCall plcioCallRead2 = new PlcioCall(IPlcioCall.PlcioMethodName.PLC_READ, "cip 192.168.1.20",
-                    "Scott_D_Conn", 1, plcTag2);
-
-            PlcioCall plcioCallRead3 = new PlcioCall(IPlcioCall.PlcioMethodName.PLC_READ, "cip 192.168.1.20",
-                    "Scott_D_Conn", 1, plcTag3);
-
-            PlcioCall plcioCallWrite1 = new PlcioCall(IPlcioCall.PlcioMethodName.PLC_WRITE, "cip 192.168.1.20",
-                    "Scott_R_Conn", 0, plcTag4);
-
-
-            PlcioCall plcioCallClose1 = new PlcioCall(IPlcioCall.PlcioMethodName.PLC_CLOSE, "cip 192.168.1.20",
-                    "Scott_R_Conn", 0);
-            PlcioCall plcioCallClose2 = new PlcioCall(IPlcioCall.PlcioMethodName.PLC_CLOSE, "cip 192.168.1.20",
-                    "Scott_D_Conn", 1);
-
-
-            // now lets try to read the compound tag
-
-            TagItem tagItemB = new TagItem("myBooleanValue", IPlcTag.PropTypes.BOOLEAN.getTypeString(), 0,
-                    PlcioPcFormat.TYPE_J, 0, 0);
-
-            TagItem tagItemS = new TagItem("myDecimalValue", IPlcTag.PropTypes.INTEGER.getTypeString(), 1,
-                    PlcioPcFormat.TYPE_J, 0, 0);
-
-            TagItem tagItemR = new TagItem("myRealValue", IPlcTag.PropTypes.REAL.getTypeString(), 2,
-                    PlcioPcFormat.TYPE_R, 0, 0);
-
-            TagItem[] tagItemsU = {tagItemB, tagItemS, tagItemR};
-
-            PlcTag plcTagU = new PlcTag("Scott_U",
-                    "" + PlcioPcFormat.TYPE_J + PlcioPcFormat.TYPE_J + PlcioPcFormat.TYPE_R,
-                    10000, 3, 12, tagItemsU);
-
-            PlcioCall plcioCallReadU = new PlcioCall(IPlcioCall.PlcioMethodName.PLC_READ, "cip 192.168.1.20",
-                    "Scott_R_Conn", 0, plcTagU);
-
-
-            master.plcAccess(plcioCallOpen1);
-            master.plcAccess(plcioCallOpen2);
-            //master.plcAccess(plcioCallRead1);
-            //master.plcAccess(plcioCallRead2);
-            //master.plcAccess(plcioCallRead3);
-            master.plcAccess(plcioCallReadU);
-
-            master.plcAccess(plcioCallWrite1);
-
-
-            master.plcAccess(plcioCallClose1);
-            master.plcAccess(plcioCallClose2);
-
-            //String readValue1 = plcTag1.getMemberValue("myRealValue");
-            //String readValue2 = plcTag2.getMemberValue("myDecimalValue");
-            //String readValue3 = plcTag3.getMemberValue("myBooleanValue");
-
-            //System.out.println("readValue1 read by client: " + readValue1);
-            //System.out.println("readValue2 read by client: " + readValue2);
-            //System.out.println("readValue3 read by client: " + readValue3);
-
-
-            String readValueU1 = plcTagU.getTagItemValue("myRealValue");
-            String readValueU2 = plcTagU.getTagItemValue("myDecimalValue");
-            String readValueU3 = plcTagU.getTagItemValue("myBooleanValue");
-
-            log.info("readValueU1 read by client: " + readValueU1);
-            log.info("readValueU2 read by client: " + readValueU2);
-            log.info("readValueU3 read by client: " + readValueU3);
-
-        } catch (Exception e) {
-            log.error("" + e);
-            e.printStackTrace();
-        }
-
-
-
-
-
-
+        log.info("Initializing ABPlcioMaster...");
+        master = new ABPlcioMaster();
+        log.info("Completed...");
 
 
     });
@@ -196,6 +82,44 @@ public class JPlcprototypeHcdHandlers extends JComponentHandlers {
 
     @Override
     public CommandResponse.SubmitResponse onSubmit(ControlCommand controlCommand) {
+
+        // here is where we do the next step
+        switch (controlCommand.commandName().name()) {
+
+            case "read":
+                log.debug("handling read command: " + controlCommand);
+
+                // code for read goes here
+
+                //String value = readTagItemValue("Scott_R", "myRealValue");
+                String value = "8.987";
+
+                Key<Double> basePosKey = JKeyType.DoubleKey().make("basePos");
+
+                Parameter<Double> basePosParam = basePosKey.set(new Double(value));
+
+                Result result = new Result("PlcHcd").add(basePosParam);
+
+                cswCtx.commandResponseManager().addOrUpdateCommand(new CommandResponse.CompletedWithResult(controlCommand.runId(), result));
+
+                break;
+
+            case "update":
+                log.debug("handling update command: " + controlCommand);
+
+                // code for read goes here
+
+                cswCtx.commandResponseManager().addOrUpdateCommand(new CommandResponse.Completed(controlCommand.runId()));
+
+                break;
+
+            default:
+                log.error("unhandled message in onSubmit: " + controlCommand);
+                // maintain actor state
+
+        }
+
+
         return new CommandResponse.Completed(controlCommand.runId());
     }
 
@@ -213,4 +137,58 @@ public class JPlcprototypeHcdHandlers extends JComponentHandlers {
     public void onGoOnline() {
 
     }
+
+
+
+    private String readTagItemValue(String tagName, String itemName) {
+
+        try {
+            // perform a test read to prove it can be done within the HCD
+            PlcioCall plcioCallOpen = new PlcioCall(IPlcioCall.PlcioMethodName.PLC_OPEN, "cip 192.168.1.20",
+                    "Scott_Conn", 0);
+
+
+            // TODO: values her will be derived from the configuration
+            TagItem tagItem1 = new TagItem(itemName, IPlcTag.PropTypes.REAL.getTypeString(), 0,
+                    PlcioPcFormat.TYPE_R, 0, 0);
+
+
+            TagItem[] tagItems1 = {tagItem1};
+
+
+            PlcTag plcTag = new PlcTag(tagName, "" + PlcioPcFormat.TYPE_R,
+                    10000, 1, 4, tagItems1);
+
+
+            PlcioCall plcioCallRead = new PlcioCall(IPlcioCall.PlcioMethodName.PLC_READ, "cip 192.168.1.20",
+                    "Scott_Conn", 0, plcTag);
+
+
+            PlcioCall plcioCallClose = new PlcioCall(IPlcioCall.PlcioMethodName.PLC_CLOSE, "cip 192.168.1.20",
+                    "Scott_Conn", 0);
+
+
+
+            master.plcAccess(plcioCallOpen);
+            master.plcAccess(plcioCallRead);
+            master.plcAccess(plcioCallClose);
+
+            String readValue = plcTag.getMemberValue(itemName);
+
+            return readValue;
+
+        } catch (Exception e) {
+            log.error("" + e);
+            e.printStackTrace();
+            return null;
+        }
+
+
+
+
+
+
+    }
+
+
 }
